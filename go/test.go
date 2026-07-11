41621844011a12820f8251cf1d6ebe640ed73b54
groxek <sashok.sashok.228@gmail.com>
2026-07-11 14:47:26 +0300

    chore(go): test slices, loops and input syntax

--- go/test.go
+++ go/test.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-var sad bool
+func main() {
+    tr := make([]string, 0,2)
+    tr = append(tr, "1")
+    tr = append(tr, "2")
+    tr = append(tr, "3")
+    fmt.Println(tr)
 
-    func main() {
-	height := 1.8
-	var wight float64 = 100
-	var IMT = wight / math.Pow(height, 2)
-	fmt.Print(IMT)
+    transactions := []float64{}
+	for {
+        transaction := get()
+        if transaction == 0 {
+            break
+        }
+        transactions = append(transactions, transaction)
+	}
+    fmt.Println(transactions)
+    totalBalance := calculateBalance(transactions)
+    fmt.Println(totalBalance)
+
 }
 
-func getUserInput() (float64, float64) {
-    var userHeight float64
-    var userKg float64
-    fmt.Scan(&userHeight)
-    fmt.Scan(&userKg)
-    return userHeight, userKg
-    
+func get() float64 {
+	var num float64
+	fmt.Println("Введите транзакцию: ")
+	fmt.Scan(&num)
+	return num
 }
 
+
+func calculateBalance(transacrions []float64) float64 {
+    var summa float64
+    for _, val := range transacrions {
+        summa += val
+    }
+    return summa
+}
