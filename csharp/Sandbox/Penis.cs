27fb347cfd2238283d30b2d31ad272da92da9603
groxek <sashok.sashok.228@gmail.com>
2026-07-15 23:28:18 +0300

    chore(csharp): init sandbox project to test syntax

---
+++ csharp/Sandbox/Penis.cs
@@ -0,0 +1,10 @@
+namespace Sandbox.Penises;
+
+internal class TestEntity
+{
+    public int A {get; set;} = 12;
+    public TestEntity(int a)
+    {
+       A = a;
+    }
+}
---
+++ csharp/Sandbox/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox.Penises;
+namespace Sandbox;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("12");
+
+        TestEntity penis = new TestEntity(123); 
+        System.Console.WriteLine(penis.A);
+    }
+
+
+}
