758db01a81dc1f153740f2b5f9e2c75a24372094
groxek <sashok.sashok.228@gmail.com>
2026-07-21 22:25:12 +0300

    chore(csharp): clean up sandbox, practice int overflow and stringbuilder

--- csharp/Sandbox/Program.cs
+++ csharp/Sandbox/Program.cs
@@ -1,15 +1,26 @@
-﻿using Sandbox.Penises;
+﻿using System.Text;
+
 namespace Sandbox;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("12");
+        int startBalance = int.MaxValue;
+        int sumTransaction = 500_000;
+        int newBalance = startBalance + sumTransaction;
 
-        TestEntity penis = new TestEntity(123); 
-        System.Console.WriteLine(penis.A);
+        System.Console.WriteLine(newBalance);
+        System.Console.WriteLine($"0x{newBalance.ToString("X")}");
+        System.Console.WriteLine($"{0:# ### ### ###}newBalance");
+        
+        string tableName = "name";
+        string targetColumn = "yo";
+        int limitValue = 4;
+
+        StringBuilder result = new StringBuilder();
+        result.Append($"SELECT {targetColumn} FROM {tableName} LIMIT {limitValue}");
+        result.Replace($"{tableName}", "newname");
+        System.Console.WriteLine(result);
     }
-
-
 }
